5136e1556200c0e8923df65412703e4ba5c248b8
sarye.haddadi <sarye@live.fr>
2022-01-08 16:18:12 +0100

    Lesson 36, client & server validation

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,29 @@ public class CategoryController : Controller
 		IEnumerable<Category> objCategoryList = _db.Categories;
 		return View(objCategoryList);
 	}
+
+	//GET
+	public IActionResult Create()
+	{
+		return View();
+	}
+
+	//POST
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public IActionResult Create(Category obj)
+	{
+		if (obj.Name == obj.DisplayOrder.ToString())
+		{
+			ModelState.AddModelError("Name", "DisplayOrder cannot exactly mach the Name.");
+		}
+		if (ModelState.IsValid)
+		{
+			_db.Categories.Add(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		return View(obj);
+	}
 }
 
