a5193ab022528dac3fc50a5a312d23d2b9f867bd
sarye.haddadi <sarye@live.fr>
2022-01-11 09:38:56 +0100

    Add areas

--- BulkyBook.Web/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Web.Controllers;
 
+[Area("Admin")]
 public class CategoryController : Controller
 {
 	private readonly IUnitOfWork _unitOfWork;
