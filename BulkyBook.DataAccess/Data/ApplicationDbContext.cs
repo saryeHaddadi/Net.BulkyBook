45b2ccc8ac43e3da1cfb1f06278604b17b5d0be1
sarye.haddadi <sarye@live.fr>
2022-01-13 10:20:17 +0100

    Add Migration, Extend IdentityUser

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ public class ApplicationDbContext : IdentityDbContext
 	public DbSet<Category> Categories { get; set; }
 	public DbSet<CoverType> CoverTypes { get; set; }
 	public DbSet<Product> Products { get; set; }
+	public DbSet<ApplicationUser> ApplicationUser { get; set; }
 
 }
 
