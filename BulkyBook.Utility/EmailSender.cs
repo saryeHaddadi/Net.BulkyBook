7e02b5184b0203f0bf73e3fb5822e61960f31501
sarye.haddadi <sarye@live.fr>
2022-01-19 12:06:46 +0100

    Add mail mockup (gmail)

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,19 @@ public class EmailSender : IEmailSender
 {
 	public Task SendEmailAsync(string email, string subject, string htmlMessage)
 	{
+		var emailToSend = new MimeMessage();
+		emailToSend.From.Add(MailboxAddress.Parse("hello@AspDotNet.fr"));
+		emailToSend.To.Add(MailboxAddress.Parse(email));
+		emailToSend.Subject = subject;
+		emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+		using (var emailClient = new SmtpClient())
+        {
+			emailClient.Connect("smtp.gmailcom", 587, MailKit.Security.SecureSocketOptions.StartTls);
+			emailClient.Authenticate("mail@gmail.com", "password");
+			emailClient.Send(emailToSend);
+		}
+
 		return Task.CompletedTask;
 	}
 }
